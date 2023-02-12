13050b96a254b5382975093f650cb88146325351
Bohdan <bodya.ru02@gmail.com>
2023-02-12 14:26:48 +0200

    Add README with links to tasks

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sofware Architecture
+
+Tasks for the Software Architecture course
+
+* [Task 1 - Microservices Basics](https://github.com/iamthewalrus67/software-architecture/tree/micro_basics)
--- internal/common/common.go
+++
@@ -1,9 +0,0 @@
-package common
-
-var FacadeServicePort string = ":8080"
-var LoggingServicePort string = ":8081"
-var MessageServicePort string = ":8082"
-
-var FacadeServiceAddress string = "http://localhost" + FacadeServicePort
-var LoggingServiceAddress string = "http://localhost" + LoggingServicePort
-var MessageServiceAddress string = "http://localhost" + MessageServicePort
